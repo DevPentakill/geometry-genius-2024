015b81767b9d56859744da995760bcb97341ac95
DevPentakill <fmahdi.bd@gmail.com>
2024-02-20 01:18:36 +0600

    created Triangle area calculator

--- scripts/triangle-calc.js
+++ scripts/triangle-calc.js
@@ -1,5 +1,22 @@
 // console.log('hellow triangle');
 
-function calculateTriangle(){
-    console.log('hellow dorittos');
+function calculateTriangle() {
+
+    const triangleBase = document.getElementById('triangle-base');
+    // console.log(triangleBase);
+    let triangleBaseValue = triangleBase.value;
+    // console.log(typeof triangleBaseValue);
+    const base = parseFloat(triangleBaseValue);
+    // console.log(typeof base);
+
+    const triangleHeight = document.getElementById('triangle-height');
+    let triangleHeightValue = triangleHeight.value;
+    const height = parseFloat(triangleHeightValue);
+
+    const triangleArea = 0.5 * base * height;
+
+    document.getElementById('triangle-area').innerText= triangleArea;
+    // triangleBaseValue = 0;
+    // triangleHeightValue = 0;
+
 }
